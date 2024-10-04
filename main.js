7f238e5f7deba9d355c6f37bcc3aafaa34fbca94
Vujkeee <157382271+Vujkeee@users.noreply.github.com>
2024-10-04 18:25:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
+//prvi zadaak
 
 const PI = 3.14;
-
 function obim(poluprecnik) {
-    const obimKruga = 2 * PI * poluprecnik;
-    console.log(`Obim kruga: ${obimKruga}`);
+    return 2 * PI * poluprecnik; 
 }
+const poluprecnik = parseFloat(prompt("Unesite poluprečnik kruga:"));
+const rezultatObima = obim(poluprecnik);
+console.log(`Obim kruga sa poluprečnikom ${poluprecnik} je: ${rezultatObima}`);
 
 
-obim(5); 
+//druggi zadaak
 
-
-const povrsinaPravougaonika = function(a, b) {
-    const povrsina = a * b;
-    console.log(`Površina pravougaonika: ${povrsina}`);
+const pravougaonik = function(a, b) {
+    return a * b; 
 };
 
-povrsinaPravougaonika(4, 6); 
+const stranicaA = parseFloat(prompt("Unesite dužinu stranice A pravougaonika:"));
+const stranicaB = parseFloat(prompt("Unesite dužinu stranice B pravougaonika:"));
+
+const povrsina = pravougaonik(stranicaA, stranicaB);
+console.log(`Površina pravougaonika sa stranicama ${stranicaA} i ${stranicaB} je: ${povrsina}`);
 
 
-function kvadrat(a) {
-    const obimKvadrata = function() {
+//3. zadatak
+const kvadrat = (a) => {
+    const obim = (function(a) {
         return 4 * a;
-    };
-    
-    const povrsinaKvadrata = function() {
+    })(a);
+    const povrsina = (function(a) {
         return a * a;
-    };
-    
-    console.log(`Obim kvadrata: ${obimKvadrata()}`);
-    console.log(`Površina kvadrata: ${povrsinaKvadrata()}`);
-}
-
-kvadrat(3); 
-
-const obimPravougaonika = (a, b) => {
-    const obim = 2 * (a + b);
-    console.log(`Obim pravougaonika: ${obim}`);
+    })(a);
+    console.log(`Obim kvadrata sa stranicom ${a} je: ${obim}`);
+    console.log(`Površina kvadrata sa stranicom ${a} je: ${povrsina}`);
 };
+const stranica = parseFloat(prompt("Unesite dužinu stranice kvadrata:"));
 
-obimPravougaonika(4, 6); 
+kvadrat(stranica);
 
-(function(ime) {
-    console.log(`Pozdrav, ${ime}!`);
-})('Marko'); 
+//cetvrti
+
+const obimPravougaonika = (duzina, sirina) => 2 * (duzina + sirina);
+
+const duzina = parseFloat(prompt("Unesite dužinu pravougaonika:"));
+const sirina = parseFloat(prompt("Unesite širinu pravougaonika:"));
+const obimP = obimPravougaonika(duzina, sirina);
+console.log(`Obim pravougaonika je: ${obimP}`);
+//peti
+(function() {
+    const name = prompt("Unesite vaše ime:");
+    console.log(`Pozdrav, ${name}!`);
+})();
